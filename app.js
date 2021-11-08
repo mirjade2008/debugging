f8e77b666129d1ec9ab35d761e0c4ab030ed9a93
mirjade2008 <mirjade2008@yahoo.com>
2021-11-07 17:12:09 -0800

    debugging

---
+++ app.js
@@ -0,0 +1,65 @@
+var a = 6;
+var b = 8;
+
+//function sum(arg1, arg2){
+    //return arg1 + arg2;
+
+//}
+//console.log(sum(a, b));
+
+//var sum = (arg1, arg2) => {
+    //return arg1 + arg2;
+//}
+//console.log(sum(a,b));
+let sum = (a, b) => a + b;
+console.log(sum(a, b));
+
+
+
+
+
+//function difference(arg1, arg2){
+    //return arg1 + arg2;
+//}
+//console.log(difference(a, b));
+
+//var difference = (arg1, arg2) => {
+    //return arg1 - arg2;
+
+//}
+//console.log(difference(a, b));
+let difference = (a, b) => a - b;
+console.log(difference(a, b));
+
+
+//function multiply(arg1, arg2){
+  //  return arg1 * arg2;
+//}
+//console.log(multiply(a, b));
+
+//var mutiply = (arg1, arg2) => {
+  //  return arg1 * arg2;
+//}
+//console.log(multiply(a, b));
+let multiply = (a, b) => a * b;
+console.log(multiply(a, b));
+
+
+
+
+//function divide(arg1, arg2){
+  //  return arg1 / arg2;
+//}
+//console.log(divide(a, b));
+
+//var divide = (arg1, arg2) => {
+  //  return arg1 / arg2;
+//}
+//console.log(divide(a, b));
+let divide = (a, b) => a / b;
+console.log(divide(a, b));
+
+
+
+
+
